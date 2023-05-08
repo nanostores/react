c6627158ea7240b5491c8e70b9071c04fdf01691
Sergey Kozlov <dartess@mail.ru>
2023-05-08 10:21:10 +0400

    remove unused types and types of deleted functions

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,5 @@
 import { Store, StoreValue } from 'nanostores'
 
-type AllKeys<T> = T extends any ? keyof T : never
-
 type StoreKeys<T> = T extends { setKey: (k: infer K, v: any) => unknown }
   ? K
   : never
@@ -40,23 +38,3 @@ export function useStore<SomeStore extends Store>(
   store: SomeStore,
   options?: UseStoreOptions<SomeStore>
 ): StoreValue<SomeStore>
-
-/**
- * Batch React updates. It is just wrap for React’s `unstable_batchedUpdates`
- * with fix for React Native.
- *
- * ```js
- * import { batch } from 'nanostores/react'
- *
- * React.useEffect(() => {
- *   let unbind = store.listen(() => {
- *     batch(() => {
- *       forceRender({})
- *     })
- *   })
- * })
- * ```
- *
- * @param cb Callback to run in batching.
- */
-export function batch(cb: () => void): void
