3d0584c18026ad44736b4ff639de7b868116ee4a
Katrin Shibakova <katrina.gubanova@gmail.com>
2024-12-18 12:46:12 +0400

    notify on change if state changed before subscription

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { listenKeys } from 'nanostores'
 import { useCallback, useRef, useSyncExternalStore } from 'react'
 
 let emit = (snapshotRef, onChange) => value => {
+  if (snapshotRef.current === value) return
   snapshotRef.current = value
   onChange()
 }
@@ -10,13 +11,13 @@ export function useStore(store, { keys, deps = [store, keys] } = {}) {
   let snapshotRef = useRef()
   snapshotRef.current = store.get()
 
-  let subscribe = useCallback(
-    onChange =>
-      keys?.length > 0
-        ? listenKeys(store, keys, emit(snapshotRef, onChange))
-        : store.listen(emit(snapshotRef, onChange)),
-    deps
-  )
+  let subscribe = useCallback(onChange => {
+    emit(snapshotRef, onChange)(store.value)
+
+    return keys?.length > 0
+      ? listenKeys(store, keys, emit(snapshotRef, onChange))
+      : store.listen(emit(snapshotRef, onChange))
+  }, deps)
   let get = () => snapshotRef.current
 
   return useSyncExternalStore(subscribe, get, get)
